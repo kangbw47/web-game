604a6938eb0b8c8b6648e32d47f9e76d45f8432f
kangbw47 <139307254+kangbw47@users.noreply.github.com>
2023-07-14 10:22:27 +0900

    Add files via upload

---
+++ game/data/mouse.js
@@ -0,0 +1,27 @@
+const mouses = [
+    {
+        id: '손',
+        cost: 0,
+        cash: 1
+    }, 
+    {
+        id: '호미',
+        cost: 30,
+        cash: 4
+    },
+    {
+        id: '괭이',
+        cost: 100,
+        cash: 12
+    },
+    {
+        id: '삽',
+        cost: 1000,
+        cash: 100
+    },
+    {
+        id: '굴착기',
+        cost: 10000,
+        cash: 500
+    },
+]
---
+++ game/data/worker.js
@@ -0,0 +1,31 @@
+const workers = [
+    {
+        id: '애벌레',
+        cost: 10,
+        cash: 1,
+        count: 0
+    }, {
+        id: '새',
+        cost: 30,
+        cash: 3,
+        count: 0
+    },
+    {
+        id: '뱀',
+        cost: 50,
+        cash: 7,
+        count: 0
+    },
+    {
+        id: '곰',
+        cost: 500,
+        cash: 70,
+        count: 0
+    },
+    {
+        id: '기계',
+        cost: 2000,
+        cash: 120,
+        count: 0
+    },
+]
